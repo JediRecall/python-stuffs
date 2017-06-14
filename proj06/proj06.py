000b7965576a7c5fcbe0a162bea82ae5a859edd0
JediRecall <peabody@pbdy-m-wc132-ap.local>
2017-06-14 13:40:19 -0500

    hangman

--- proj06/proj06.py
+++ proj06/proj06.py
@@ -46,3 +46,41 @@ def choose_word(wordlist):
 wordlist = load_words()
 
 # your code begins here!
+def hangman():
+    word = choose_word(wordlist)
+    print word
+    list2 = []
+    list3 = []
+    n = len(word)
+    for ltr in word:
+        list2.append ("_")
+        list3.append (ltr)
+    print list2
+    g = 6
+    while g > 0:
+        g = (g-1)
+        print "you have"
+        print g
+        print "guesses left"
+        usript1 = raw_input("guess a letter (no caps): ")
+        if len(usript1) > 1:
+            print "I said one letter"
+            continue
+        if usript1 in word:
+            print "you guessededed correct!"
+            g = (g+1)
+            for num in range (len(word)):
+                if usript1 == word[num]:
+                    list2 [num] = usript1
+            if list2 == list3:
+                print "you win"
+                print list3
+                break
+            print list2
+        else:
+            print "Wrong! try aganz"
+            if g == -1:
+                print "you're all out of guesses"
+                break
+
+hangman()
